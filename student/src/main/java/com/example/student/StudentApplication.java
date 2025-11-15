24bea969b28b5205eacc43a3a8a6861e20bacb63
Bharathpusarla <bharathpusarla52@gmail.com>
2025-11-15 15:37:16 +0530

    JMP:66834

--- student/src/main/java/com/example/student/StudentApplication.java
+++ student/src/main/java/com/example/student/StudentApplication.java
@@ -10,4 +10,17 @@ public class StudentApplication {
 		SpringApplication.run(StudentApplication.class, args);
 	}
 
+	public static void findDuplicates(String input) {
+		HashMap<Character, Integer> chars = new HashMap<>();
+		for (Character c : input.toCharArray()) {
+			chars.put(c, chars.getOrDefault(c, 0) + 1);
+		}
+		for (Map.Entry<Character, Integer> i : chars.entrySet()) {
+			if (i.getValue() > 1) {
+				//System.out.println(i.getKey() + " appears " + i.getValue() + " times");
+				System.out.print(i.getKey());
+			}
+		}
+
+	}
 }
